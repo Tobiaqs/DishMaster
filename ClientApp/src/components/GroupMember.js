6d175c9ddb6e27126930c8175f7ec86281ffd7e2
Tobias Sijtsma <tobias@tobiass.nl>
2018-10-11 23:13:34 +0200

    Implement group member page

--- ClientApp/src/components/GroupMember.js
+++ ClientApp/src/components/GroupMember.js
@@ -1,7 +1,81 @@
+import { Redirect } from 'react-router';
 import React, { Component } from 'react';
+import { Badge, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Api } from '../Api';
+import { ModalConfirm } from './ModalConfirm';
 
 export class GroupMember extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            groupMember: null,
+            deletingGroupMember: false,
+            groupMemberDeleted: false
+        };
+        
+        this.deleteGroupMember = this.deleteGroupMember.bind(this);
+        this.onModalHide = this.onModalHide.bind(this);
+        this.onModalConfirmed = this.onModalConfirmed.bind(this);
+    }
+
+    fetch() {
+        Api.getInstance().Group.GetGroupMember({ groupMemberId: this.props.match.params.groupMemberId }).then(result => {
+            this.setState({ groupMember: result.payload });
+        });
+    }
+    
+    onModalHide() {
+        this.setState({ deletingGroupMember: false });
+    }
+
+    onModalConfirmed() {
+        Api.getInstance().Group.DeleteGroupMember({ groupMemberId: this.props.match.params.groupMemberId }).then(result => {
+            this.onModalHide();
+            if (result.succeeded) {
+                this.setState({ groupMemberDeleted: true });
+            } else {
+                alert("Failed!");
+            }
+        });
+    }
+
+    deleteGroupMember() {
+        this.setState({ deletingGroupMember: true });
+    }
+
+    componentDidMount() {
+        this.fetch();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.match.params.groupMemberId !== this.props.match.params.groupMemberId) {
+            this.setState({ groupMember: null });
+            this.fetch();
+        }
+    }
+
+    getGroupMemberName() {
+        return this.state.groupMember.isAnonymous ? this.state.groupMember.anonymousName : this.state.groupMember.fullName;
+    }
+
     render() {
-        return <div />;
+        return <div>
+            {this.state.groupMember ? <div>
+                <h1>{this.getGroupMemberName()}{this.state.groupMember.administrator ? <span> <Badge>admin</Badge></span> : null}</h1>
+                <h4>Score</h4>
+                <p>{this.getGroupMemberName()} heeft een score van {this.state.groupMember.score} punten.</p>
+                <h4>Administratie</h4>
+                <ListGroup>
+                    <ListGroupItem onClick={this.deleteGroupMember}><i>Dit groepslid verwijderen&#8230;</i></ListGroupItem>
+                </ListGroup>
+            </div> : <h1>Laden&#8230;</h1>}
+            <ModalConfirm
+                show={this.state.deletingGroupMember}
+                message="Weet je zeker dat je dit groepslid wilt verwijderen?"
+                onHide={this.onModalHide}
+                onConfirmed={this.onModalConfirmed} />
+            {this.state.groupMemberDeleted ? <Redirect to={'/group/' + this.props.match.params.groupId} push={false} /> : null}
+        </div>;
     }
 }
