aa506260d11e211a08da2af90fd2e575070c0536
Tobias Sijtsma <tobias@tobiass.nl>
2018-10-11 14:52:45 +0200

    Create all routes, provide contexts at the top level

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,14 +1,81 @@
-﻿import React from 'react';
+import { LS_AUTH_TOKEN, Api, AuthContext } from './Api';
+import React, { Component } from 'react';
 import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import { Layout } from './components/Layout';
+import { GroupListContext } from './components/NavMenu';
+import { Home } from './components/Home';
+import { Login } from './components/Login';
+import { LinkGroup } from './components/LinkGroup';
+import { LinkGroupQR } from './components/LinkGroupQR';
+import { Logout } from './components/Logout';
+import { Group } from './components/Group';
+import { LinkGroupMember } from './components/LinkGroupMember';
+import { GroupMember } from './components/GroupMember';
+import { TaskGroup } from './components/TaskGroup';
+import { Task } from './components/Task';
+import { Register } from './components/Register';
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetchdata/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      auth: {
+        loggedIn: !!localStorage.getItem(LS_AUTH_TOKEN),
+        setAuthToken: this.setAuthToken.bind(this)
+      },
+      
+      groupList: {
+        needsUpdate: false,
+        setNeedsUpdate: this.setNeedsUpdate.bind(this)
+      }
+    };
+  }
+
+  setNeedsUpdate(value) {
+    this.setState({
+      auth: {
+        ...this.state.auth
+      },
+      groupList: {
+        ...this.state.groupList,
+        needsUpdate: value
+      }
+    });
+  }
+
+  setAuthToken(authToken) {
+    // Store token for use by Api
+    Api.getInstance().persistAuthToken(authToken);
+
+    this.setState({
+      auth: {
+        ...this.state.auth,
+        loggedIn: !!authToken
+      },
+      groupList: {
+        ...this.state.groupList
+      }
+    });
+  }
+
+  render() {
+    return <GroupListContext.Provider value={this.state.groupList}>
+      <AuthContext.Provider value={this.state.auth}>
+        <Layout>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/logout' component={Logout} />
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/link-group-qr' component={LinkGroupQR} />
+          <Route exact path='/link-group/:invitationSecret' component={LinkGroup} />
+          <Route exact path='/group/:groupId' component={Group} />
+          <Route exact path='/group/:groupId/link-group-member' component={LinkGroupMember} />
+          <Route exact path='/group/:groupId/taskGroup/:taskGroupId' component={TaskGroup} />
+          <Route exact path='/group/:groupId/taskGroup/:taskGroupId/task/:taskId' component={Task} />
+          <Route exact path='/group/:groupId/groupMember/:groupMemberId' component={GroupMember} />
+        </Layout>
+      </AuthContext.Provider>
+    </GroupListContext.Provider>;
+  }
+}
