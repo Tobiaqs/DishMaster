4ce456ea29cba22a31800a4b24a92a4600eb1400
Tobias Sijtsma <tobias@tobiass.nl>
2018-10-29 22:59:53 +0100

    Deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 rm -rf bin/Release
 dotnet publish -c Release
 cd bin/Release/netcoreapp2.1
-zip -r ../../../build.zip publish/
-cd ../../../
-scp build.zip tobiass.nl:
-ssh tobiass.nl ~/stop_wdda.sh
+mv publish/ wdda.env/
+zip -rq deployment.zip wdda.env/
+scp deployment.zip tobiass.nl:
+ssh root@tobiass.nl systemctl stop wdda
 ssh tobiass.nl mv ~/wdda.env/wdda.db ~/wdda.db
 ssh tobiass.nl rm -rf ~/wdda.env/
-ssh tobiass.nl unzip build.zip
-ssh tobiass.nl mv ~/publish/ ~/wdda.env/
+ssh tobiass.nl unzip ~/deployment.zip
+ssh tobiass.nl rm ~/deployment.zip
 ssh tobiass.nl mv ~/wdda.db ~/wdda.env/wdda.db
-rm build.zip
-ssh tobiass.nl rm ~/build.zip
-ssh tobiass.nl ~/start_wdda.sh
+cd ../../../
+rm -rf bin/Release
+ssh root@tobiass.nl systemctl start wdda
