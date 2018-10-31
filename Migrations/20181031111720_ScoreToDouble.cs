309c7339aea346f3a4984427171762251eb99e3b
Tobias Sijtsma <tobias@tobiass.nl>
2018-10-31 12:21:31 +0100

    Create migration to upgrade the database to use double as type for Score

---
+++ Migrations/20181031111720_ScoreToDouble.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace wiedoetdeafwas.Migrations
+{
+    public partial class ScoreToDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Score",
+                table: "GroupMembers",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Score",
+                table: "GroupMembers",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
