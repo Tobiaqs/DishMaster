bb60905082b9b475d70764aa111eb32832803239
Tobias Sijtsma <tobias@tobiass.nl>
2018-10-31 12:29:05 +0100

    Deploy script aestetics

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,10 @@ zip -rq deployment.zip wdda.env/
 scp deployment.zip tobiass.nl:
 ssh root@tobiass.nl systemctl stop wdda
 ssh tobiass.nl rm -rf ~/wdda.env/
-ssh tobiass.nl unzip ~/deployment.zip
+ssh tobiass.nl unzip -qq ~/deployment.zip
 ssh tobiass.nl rm ~/deployment.zip
 cd ../../../
 rm -rf bin/Release
 ssh root@tobiass.nl systemctl start wdda
+echo -e "\n😊 Deploy script finished"
+echo "👋 Bye!"
