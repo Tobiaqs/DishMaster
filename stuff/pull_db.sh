7756ef425d9433c57226f9e1d3b660989beeaa16
Tobias Sytsma <tobias@tobiass.nl>
2021-04-02 10:32:24 +0200

    Delete old scripts

--- stuff/pull_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mysql -u root -pAepahng0aw0aeZeeyoothohLae0quo < stuff/recreate_db.sql
-ssh tobiass.nl mysqldump -u wdda -pieVeteejot6co0reil6ahLei0ohPaa wdda > wdda_pull.sql
-mysql -u wdda -pEeph9Wae2kooWoochozouh7jo2caj5 -D wdda < wdda_pull.sql
-#rm wdda_pull.sql
-echo -e "\n😊 Database pull finished"
-echo "👋 Bye!"
--- stuff/recreate_db.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mysql -u root -pAepahng0aw0aeZeeyoothohLae0quo < stuff/recreate_db.sql
-dotnet ef database update
-echo -e "\n😊 Database recreation finished"
-echo "👋 Bye!"
